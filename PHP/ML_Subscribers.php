6b850642b74edb1688befc9268e628ff3566a5df
Eimantas Norkunas <eimantas@mailerlite.com>
2013-06-13 18:14:00 +0300

    REST

---
+++ PHP/ML_Subscribers.php
@@ -0,0 +1,52 @@
+<?php
+	
+	require_once dirname(__FILE__).'/base/ML_Rest.php';
+	
+	class ML_Subscribers extends ML_Rest
+	{
+		function ML_Subscribers( $api_key )
+		{	
+			$this->name = 'subscribers';
+
+			parent::__construct($api_key);
+		}
+
+		function add( $subscriber, $resubscribe = 0 )
+		{
+			$subscriber['resubscribe'] = $resubscribe;
+
+			return $this->execute( 'POST', $subscriber );
+		}
+
+		function addAll( $subscribers, $resubscribe = 0 )
+		{
+			$data['resubscribe'] = $resubscribe;
+
+			$data['subscribers'] = $subscribers;
+
+			$this->path .= 'import/';
+
+			return $this->execute( 'POST', $data );
+		}
+
+		function get( $email, $history = 0 )
+		{
+			$this->setId( null );
+
+			$this->path .= '?email=' . urlencode( $email );
+
+			if ( $history )
+				$this->path .= '&history=1';
+
+			return $this->execute( 'GET' );
+		}
+
+		function remove( $email )
+		{
+			$this->path .= '?email=' . urlencode( $email );
+
+			return $this->execute( 'DELETE' );
+		}
+	}
+
+?>
